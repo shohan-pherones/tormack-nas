93d538b6914362534b38cf01c86ae2bef73ea6e3
shohan-pherones <shohan.sub.56@gmail.com>
2023-06-08 00:06:40 +0600

    admin added

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+const mongoose = require("mongoose");
 
 // jwt token
 const createToken = (_id) => {
@@ -59,7 +60,110 @@ const loginUser = async (req, res) => {
   }
 };
 
+// get all users
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    if (!users) {
+      throw Error("Users not found.");
+    }
+
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
+
+// get an user
+const getUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      throw Error("User not found.");
+    }
+
+    if (userId !== req.user?._id.toString()) {
+      throw Error("Unauthorized access.");
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw Error("User not found.");
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
+
+// delete an user
+const deleteUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      throw Error("User not found.");
+    }
+
+    if (userId !== req.user?._id.toString()) {
+      throw Error("Unauthorized access.");
+    }
+
+    const user = await User.findByIdAndDelete(userId);
+
+    if (!user) {
+      throw Error("User not found.");
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
+
+// update an user
+const updateUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { name } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      throw Error("User not found.");
+    }
+
+    if (userId !== req.user?._id.toString()) {
+      throw Error("Unauthorized access.");
+    }
+
+    if (!name) {
+      throw Error("Name field must be filled.");
+    }
+
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { $set: { name } },
+      { new: true }
+    );
+
+    if (!user) {
+      throw Error("User not found.");
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
+
 module.exports = {
   signupUser,
   loginUser,
+  getUser,
+  getUsers,
+  deleteUser,
+  updateUser,
 };
